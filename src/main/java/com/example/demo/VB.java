09b1fa7306fd193d2764088e4c37bb6192aea199
shalendra <shalendrashalendrasahu703@gmail>
2025-03-18 15:11:35 +0530

    hello

--- src/main/java/com/example/demo/VB.java
+++ src/main/java/com/example/demo/VB.java
@@ -1,14 +1,12 @@
 package com.example.demo;
 
 public class VB {
-<<<<<<< Updated upstream
 
     public String reverseString(String str) {
         return new StringBuilder(str).reverse().toString();
     }
-=======
-//    public int addNumbers(int a, int b) {
-//        return a + b;
-//    }
->>>>>>> Stashed changes
+    public int addNumbers(int a, int b) {
+
+   return a + b;
+}
 }
