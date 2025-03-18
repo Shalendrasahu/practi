7bf1395c7d42140bee635451e9579a6c4933c2f9
Shalendra sahu <163658786+Shalendrasahu@users.noreply.github.com>
2025-03-18 12:15:06 +0530

    Update A.java

--- src/main/java/com/example/demo/A.java
+++ src/main/java/com/example/demo/A.java
@@ -3,6 +3,7 @@ package com.example.demo;
 public class A {
     public void methodA() {
         System.out.println("A method");
+        System.out.println(100);
     }
     public void main(){
           System.out.println("B method");
